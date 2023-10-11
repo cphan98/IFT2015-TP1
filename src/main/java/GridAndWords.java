e8460f5d53b547efe6db2b0c738692ccd7b92f1f
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-10-11 15:58:43 -0400

    change 'chars' to 'grid' in GridAndWords.java

--- src/main/java/GridAndWords.java
+++ src/main/java/GridAndWords.java
@@ -1,35 +1,39 @@
 import java.util.Arrays;
 
+// GridAndWords represents the grid and list of words given for each query.
 public class GridAndWords {
-    private char[][] chars;
+    // attributes
+    private char[][] grid;
     private String[] words;
 
-    public GridAndWords(char[][] chars, String[] words) {
-        this.chars = chars;
+    // constructor
+    public GridAndWords(char[][] grid, String[] words) {
+        this.grid = grid;
         this.words = words;
     }
 
-    public char[][] getChars() {
-        return chars;
+    // getters
+    public char[][] getGrid() {
+        return grid;
     }
-
     public String[] getWords() {
         return words;
     }
 
+    // setters
+    public void setGrid(char[][] grid) {
+        this.grid = grid;
+    }
+    public void setWords(String[] words) {
+        this.words = words;
+    }
+
+    // pretty print
     @Override
     public String toString() {
         return "GridAndWords {" +
-                "chars=" + Arrays.deepToString(chars) +
+                "grid=" + Arrays.deepToString(grid) +
                 ", words=" + Arrays.toString(words) +
                 '}';
-    }
-
-    public void setChars(char[][] chars) {
-        this.chars = chars;
-    }
-
-    public void setWords(String[] words) {
-        this.words = words;
     }
 }
