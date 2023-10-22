0ca2b942ba729d499a679f9da45d6cde6e4aff21
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-10-22 16:24:48 -0400

    Update Position.java
    add Position.java

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,14 +1,18 @@
 import java.lang.IllegalStateException;
 
 /**
- * Position is an interface for the Position ADT
- *
- * Source : <a href="https://udemontreal-my.sharepoint.com/personal/francois_major_umontreal_ca/_layouts/15/onedrive.aspx?sortField=LinkFilename&isAscending=true&FolderCTID=0x012000058D0A760FE89C4BA8A2D96D83CF11CC&id=%2Fpersonal%2Ffrancois%5Fmajor%5Fumontreal%5Fca%2FDocuments%2FIFT2015%2FNotes%2FCode%2Fsrc%2Fmain%2Fjava%2Fca%2Fumontreal%2Fadt%2Flist%2FPosition%2Ejava&parent=%2Fpersonal%2Ffrancois%5Fmajor%5Fumontreal%5Fca%2FDocuments%2FIFT2015%2FNotes%2FCode%2Fsrc%2Fmain%2Fjava%2Fca%2Fumontreal%2Fadt%2Flist">Position.java</a>
- */
-public interface Position<E> {
-    // return element stored at this Position
-    E getElement() throws IllegalStateException;
+* Position is an interface for the Position ADT
+* 
+* Based on Goodrich, Tamassia & Goldwasser
+*
+* @author      Francois Major
+* @version     %I%, %G%
+* @since       1.0
+*/
 
+public interface Position<E> {
+    // return the element stored at this Position
+    E getElement() throws IllegalStateException;
     // return the container of this Position
     Object getContainer();
 }
