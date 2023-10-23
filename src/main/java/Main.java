5cf61ecb0bdef7f01e100f3b7cd3b0b1ee5500ca
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-10-23 18:57:25 -0400

    edit Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
